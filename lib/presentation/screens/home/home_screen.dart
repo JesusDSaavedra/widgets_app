e3198cd4f5259ad0191b8b67d5a400bce5d14de6
JesusDSaavedra <iam.jesusd88@gmail.com>
2025-10-19 01:27:53 -0500

    feat: implement go_router for navigation

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -41,9 +42,7 @@ class _CustomListTitle extends StatelessWidget {
       leading: Icon(menuItem.icon, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
-      onTap: () {
-        Navigator.pushNamed(context, menuItem.link);
-      },
+      onTap: () => context.go(menuItem.link),
     );
   }
 }
