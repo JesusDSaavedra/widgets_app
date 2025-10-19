cd10dc49b4a62686fb69a53d9a57e59ce87dc948
JesusDSaavedra <iam.jesusd88@gmail.com>
2025-10-19 00:40:37 -0500

    feat: implement home screen with dynamic menu items and custom list tiles

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Flutter + Material 3')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter + Material 3')),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final MenuItem item = appMenuItems[index];
+        return _CustomListTitle(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({required this.item});
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary),
+      leading: Icon(item.icon, color: colors.primary),
+      title: Text(item.title),
+      subtitle: Text(item.subtitle),
+    );
   }
 }
